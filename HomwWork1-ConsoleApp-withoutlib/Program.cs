4eb01119ccf4c73832acccb73f9e32e556f65fbf
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2023-10-12 16:33:40 +0300

    Add project files.

---
+++ HomwWork1-ConsoleApp-withoutlib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomwWork1_ConsoleApp_withoutlib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            if (name == String.Empty)
+                name = "No name";
+            Console.WriteLine("Welcome {0} to dotnet world", name);
+        }
+    }
+}
